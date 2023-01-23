e116a7fa390642b1f89035a3e6edae7ebc6d9377
Alyssa Harris <ah00214@my.westga.edu>
2023-01-23 16:43:58 -0500

    Added comments and closing logic

--- TechSupport/View/MainForm.cs
+++ TechSupport/View/MainForm.cs
@@ -8,13 +8,41 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace TechSupport.View
 {
+    /// <summary>
+    /// Mainform Class used to display the username of the tech support application
+    /// Author: Alyssa Harris
+    /// Version: 1/23/23
+    /// </summary>
     public partial class MainForm : Form
     {
+        private LoginForm loginForm;
+
+        /// <summary>
+        /// Initializes a new instance of the MainForm class.
+        /// </summary>
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(Object sender, EventArgs e)
+        {
+            usernameLabel.Text = "Jane";
+        }
+        private void LogoutLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            loginForm = new LoginForm();
+            loginForm.Closed += (s, args) => this.Close();
+            loginForm.Show();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
