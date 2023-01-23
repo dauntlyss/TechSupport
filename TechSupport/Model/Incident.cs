93fb74b1bb40067a85c8d25ad5eced44c64e4136
Alyssa Harris <ah00214@my.westga.edu>
2023-01-23 17:16:33 -0500

    Added comments to constructor

--- TechSupport/Model/Incident.cs
+++ TechSupport/Model/Incident.cs
@@ -24,6 +24,18 @@ namespace TechSupport.Model
         /// </summary>
         public string Description { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the Incident class.
+        /// </summary>
+        /// <param name="customerId">The customer identifier.</param>
+        /// <param name="title">The title.</param>
+        /// <param name="description">The description.</param>
+        /// <exception cref="System.ArgumentOutOfRangeException">customerId - Customer ID for the incident must be greater than 0.</exception>
+        /// <exception cref="System.ArgumentException">
+        /// The title of the incident cannot be empty or exceed 100 characters. - title
+        /// or
+        /// The description of the incident cannot be empty or greater than 300 characters. - description
+        /// </exception>
         public Incident(int customerId, string title, string description)
         {
             if (customerId < 0)
