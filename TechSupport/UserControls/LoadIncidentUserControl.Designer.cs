56b6b6731293deca4f820c4231a4ab552c283900
Alyssa Harris <ah00214@my.westga.edu>
2023-02-06 21:23:44 -0500

    Updated loadIncident tab with working user control

--- TechSupport/UserControls/LoadIncidentUserControl.Designer.cs
+++ TechSupport/UserControls/LoadIncidentUserControl.Designer.cs
@@ -28,18 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.incidentDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.incidentDataGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // incidentDataGridView
+            // 
+            this.incidentDataGridView.AllowUserToAddRows = false;
+            this.incidentDataGridView.AllowUserToDeleteRows = false;
+            this.incidentDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.incidentDataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.incidentDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.incidentDataGridView.DefaultCellStyle = dataGridViewCellStyle2;
+            this.incidentDataGridView.Location = new System.Drawing.Point(3, 3);
+            this.incidentDataGridView.Name = "incidentDataGridView";
+            this.incidentDataGridView.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.incidentDataGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            this.incidentDataGridView.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.incidentDataGridView.Size = new System.Drawing.Size(699, 424);
+            this.incidentDataGridView.TabIndex = 5;
+            this.incidentDataGridView.VisibleChanged += new System.EventHandler(this.IncidentDataGridView_VisibleChanged);
             // 
             // LoadIncidentUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.incidentDataGridView);
             this.Name = "LoadIncidentUserControl";
             this.Size = new System.Drawing.Size(705, 430);
+            ((System.ComponentModel.ISupportInitialize)(this.incidentDataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView incidentDataGridView;
     }
 }
--- TechSupport/UserControls/LoadIncidentUserControl.cs
+++ TechSupport/UserControls/LoadIncidentUserControl.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TechSupport.Controller;
 
 namespace TechSupport.UserControls
 {
+    /// <summary>
+    /// User COntrol class that encapsulates the load all incidents controls
+    /// Author: Alyssa Harris
+    /// Version: 02/07/2023
+    /// </summary>
     public partial class LoadIncidentUserControl : UserControl
     {
+        #region Data members
+
+        private readonly IncidentController incidentController;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// constructor used to create the add incident controls
+        /// </summary>
         public LoadIncidentUserControl()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            this.incidentController = new IncidentController();
         }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// method used to refresh and load all incidents
+        /// </summary>
+        public void RefreshIncidentDataGrid()
+        {
+            incidentDataGridView.DataSource = null;
+            incidentDataGridView.DataSource = this.incidentController.GetAllIncidents();
+        }
+
+        private void IncidentDataGridView_VisibleChanged(object sender, EventArgs e)
+        {
+            RefreshIncidentDataGrid();
+        }
+
+        #endregion
     }
 }
