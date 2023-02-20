2599df7f53a943e8c141bf1e93dbe7304afaa364
Alyssa Harris <ah00214@my.westga.edu>
2023-02-20 17:11:34 -0500

    Added a DB statement that returns a single customer by customerrID

--- TechSupport/DAL/CustomerDBDAL.cs
+++ TechSupport/DAL/CustomerDBDAL.cs
@@ -13,6 +13,57 @@ namespace TechSupport.DAL
     public class CustomerDBDAL
     {
         #region Methods
+
+        /// <summary>
+        /// This method connects to the database and runs a query to return customer by customerID
+        /// </summary>
+        /// <param name="customerID">customer id</param>
+        /// <returns>single customer in list that matches the customerID</returns>
+        public List<Customer> GetCustomer(int customerID)
+        {
+            List<Customer> customerList = new List<Customer>();
+
+            string selectStatement =
+                "SELECT * " +
+                "FROM Customers " +
+                "WHERE " +
+                "CustomerID = @customerID";
+
+            using (SqlConnection connection = TechSupportDBConnection.GetConnection())
+            {
+                connection.Open();
+
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.Add("@customerID", System.Data.SqlDbType.Int);
+                    selectCommand.Parameters["@customerID"].Value = customerID;
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Customer customer = new Customer
+                            {
+                                CustomerID = (int)reader["CustomerID"],
+                                Name = reader["Name"].ToString(),
+                                Address = reader["Address"].ToString(),
+                                City = reader["City"].ToString(),
+                                State = reader["State"].ToString(),
+                                ZipCode = reader["ZipCode"].ToString(),
+                                Phone = reader["Phone"].ToString(),
+                                Email = reader["Email"].ToString()
+                            };
+                            customerList.Add(customer);
+                        }
+                    }
+                }
+            }
+            return customerList;
+        }
+
+        /// <summary>
+        /// This method connect to the DB and runs a query to return the customer ID and names
+        /// </summary>
+        /// <returns>A list of al customer's ids and names</returns>
         public List<CustomerIdAndName> GetAllCustomerIdAndNames()
         {
             string selectStatement =
